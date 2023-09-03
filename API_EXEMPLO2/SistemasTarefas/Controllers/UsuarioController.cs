e2a6837d9702257edd165276fa253be1aec02303
Leonardo Romeiro <leozinwork@gmail.com>
2023-09-03 20:49:51 -0300

    Adicionando exemplo 2

---
+++ API_EXEMPLO2/SistemasTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemasTarefas.Models;
+
+namespace SistemasTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet] public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios() {
+            return Ok();
+        }
+    }
+}
